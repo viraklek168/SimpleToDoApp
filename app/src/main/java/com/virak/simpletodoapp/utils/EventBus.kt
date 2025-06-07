338dfcf85b1707408eec256117942b40b992e732
Virak <viraklek168@gmail.com>
2025-06-07 10:07:49 +0700

    implement ripple when click on day

---
+++ app/src/main/java/com/virak/simpletodoapp/utils/EventBus.kt
@@ -0,0 +1,19 @@
+package com.virak.simpletodoapp.utils
+
+object SimpleEventBus {
+    private val listeners = mutableListOf<(Any) -> Unit>()
+
+    fun post(event: Any) {
+        listeners.forEach { it(event) }
+    }
+
+    fun subscribe(listener: (Any) -> Unit) {
+        listeners.add(listener)
+    }
+
+    fun unsubscribe(listener: (Any) -> Unit) {
+        listeners.remove(listener)
+    }
+}
+
+data class DayClickedEvent(val day: String)
--- app/src/main/java/com/virak/simpletodoapp/utils/adapter/CalendarDayAdapter.kt
+++ app/src/main/java/com/virak/simpletodoapp/utils/adapter/CalendarDayAdapter.kt
@@ -4,7 +4,10 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.virak.simpletodoapp.MyCalendarDay
+import com.virak.simpletodoapp.R
 import com.virak.simpletodoapp.databinding.ItemCalendarCellBinding
+import com.virak.simpletodoapp.utils.DayClickedEvent
+import com.virak.simpletodoapp.utils.SimpleEventBus
 
 class CalendarDayAdapter(
     private val listDays: List<MyCalendarDay>
@@ -15,6 +18,12 @@ class CalendarDayAdapter(
 
         fun bind(day: MyCalendarDay) {
             binding.tvLabel.text = if(day.day == 0) "" else day.day.toString()
+            binding.tvLabel.setOnClickListener {
+                SimpleEventBus.post(DayClickedEvent(day = day.day.toString()))
+            }
+            if(day.isCurrent){
+                binding.tvLabel.setBackgroundResource(R.drawable.bg_current_day)
+            }
         }
     }
 
